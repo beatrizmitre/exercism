e52bef4eb871dabb2222e689e739549453ced251
Beatriz Mitre <beatrizmmitre@gmail.com>
2025-01-21 16:22:56 -0300

    armstrong numbers function working

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -3,6 +3,16 @@
 // convenience to get you started writing code faster.
 //
 
-export const isArmstrongNumber = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isArmstrongNumber = (number) => {
+  let numberChars = String(number).split('').map((num) => Number(num))
+  let numberOfDigits = numberChars.length
+  let totalSum = 0
+
+  let numbersToSum = numberChars.map((num) => num ** numberOfDigits)
+  for (let i = 0; i < numbersToSum.length; i++) {
+    totalSum = totalSum + numbersToSum[i]
+  }
+  console.log(number)
+  console.log(totalSum)
+  return number == totalSum
 };
