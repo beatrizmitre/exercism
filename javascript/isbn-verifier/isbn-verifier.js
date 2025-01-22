16a478ea816a5ad2bc2567e79c20db831240a23c
Beatriz Mitre <beatrizmmitre@gmail.com>
2025-01-22 16:20:17 -0300

    only test 17 is failing

--- javascript/isbn-verifier/isbn-verifier.js
+++ javascript/isbn-verifier/isbn-verifier.js
@@ -3,6 +3,35 @@
 // convenience to get you started writing code faster.
 //
 
-export const isValid = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isValid = (string) => {
+  let arrayOfDigits = []
+  let splitString = string.split('')
+
+  for (let d = 0; d < splitString.length; d++) {
+    let itemAsNumber = Number(splitString[d])
+    if (itemAsNumber || itemAsNumber == 0) {
+      arrayOfDigits.push(itemAsNumber)
+    }
+    else if ((splitString[d] == 'X')) {
+      if (d == (splitString.length - 1)) {
+        arrayOfDigits.push(10)
+      }
+      else {
+        return false
+      }
+    }
+  }
+
+  console.log(arrayOfDigits)
+
+  if (arrayOfDigits.length < 10 || arrayOfDigits.length > 10 || arrayOfDigits.length == 0) {
+    return false
+  }
+
+  let totalSum = 0
+  for (let i = 0; i < arrayOfDigits.length; i++) {
+    totalSum += arrayOfDigits[i] * (arrayOfDigits.length - i)
+  }
+
+  return totalSum % 11 == 0
 };
